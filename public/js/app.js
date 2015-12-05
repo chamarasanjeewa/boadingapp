6b26f6ca87d4cfdbf197d2f32a07ff2855c3dbe5
chamarasanjeewa <chamara.sanjeewa@gmail.com>
2015-12-05 05:10:08 -0800

    make

--- public/js/app.js
+++ public/js/app.js
@@ -71,7 +71,19 @@ angular.module('ionicApp', ['ionic'])
    $urlRouterProvider.otherwise('/sign-in');
 
 })
-
+.factory('Todos', ['$http',function($http) {
+    return {
+      get : function() {
+        return $http.get('/api/todos');
+      },
+      create : function(todoData) {
+        return $http.post('/api/todos', todoData);
+      },
+      delete : function(id) {
+        return $http.delete('/api/todos/' + id);
+      }
+    }
+  }])
 .controller('SignInCtrl', function($scope, $state) {
   
   $scope.signIn = function(user) {
@@ -81,6 +93,39 @@ angular.module('ionicApp', ['ionic'])
   
 })
 
-.controller('HomeTabCtrl', function($scope) {
-  console.log('HomeTabCtrl');
-});
+.controller('HomeTabCtrl', function($scope,Todos) {
+  $scope.formData = {};
+		$scope.loading = true;
+
+		// GET =====================================================================
+		// when landing on the page, get all todos and show them
+		// use the service to get all the todos
+		Todos.get()
+			.success(function(data) {
+				$scope.todos = data;
+				$scope.loading = false;
+			});
+
+		// CREATE ==================================================================
+		// when submitting the add form, send the text to the node API
+		$scope.createNewGood = function() {
+
+			// validate the formData to make sure that something is there
+			// if form is empty, nothing will happen
+			if ($scope.formData.text != undefined) {
+				$scope.loading = true;
+
+				// call the create function from our service (returns a promise object)
+				Todos.create($scope.formData)
+
+					// if successful creation, call our get function to get all the new todos
+					.success(function(data) {
+						$scope.loading = false;
+						$scope.formData = {}; // clear the form so our user is ready to enter another
+						$scope.todos = data; // assign our new list of todos
+					});
+			}
+		};
+
+  
+  })
