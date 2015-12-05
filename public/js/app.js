fc35b4cd844c57e2106a7595b7bd335dfbcab7a2
chamarasanjeewa <chamara.sanjeewa@gmail.com>
2015-12-05 07:10:50 -0800

    added

--- public/js/app.js
+++ public/js/app.js
@@ -26,6 +26,15 @@ angular.module('ionicApp', ['ionic'])
         }
       }
     })
+    .state('tabs.list', {
+      url: '/list',
+      views: {
+        'home-tab': {
+          templateUrl: 'templates/purchasedList.html',
+          controller: 'listCtrl'
+        }
+      }
+    })
     .state('tabs.facts', {
       url: '/facts',
       views: {
@@ -71,7 +80,7 @@ angular.module('ionicApp', ['ionic'])
    $urlRouterProvider.otherwise('/sign-in');
 
 })
-.factory('Todos', ['$http',function($http) {
+.factory('PurchaseService', ['$http',function($http) {
     return {
       get : function() {
         return $http.get('/api/todos');
@@ -93,22 +102,22 @@ angular.module('ionicApp', ['ionic'])
   
 })
 
-.controller('HomeTabCtrl', function($scope,Todos) {
+.controller('HomeTabCtrl', function($scope,$state,PurchaseService) {
   $scope.formData = {};
 		$scope.loading = true;
 
 		// GET =====================================================================
 		// when landing on the page, get all todos and show them
 		// use the service to get all the todos
-		Todos.get()
+		PurchaseService.get()
 			.success(function(data) {
-				$scope.todos = data;
+				$scope.itemList = data;
 				$scope.loading = false;
 			});
 
 		// CREATE ==================================================================
 		// when submitting the add form, send the text to the node API
-		$scope.createNewGood = function() {
+		$scope.createPurchaseItem = function() {
 
 			// validate the formData to make sure that something is there
 			// if form is empty, nothing will happen
@@ -116,16 +125,38 @@ angular.module('ionicApp', ['ionic'])
 				$scope.loading = true;
 
 				// call the create function from our service (returns a promise object)
-				Todos.create($scope.formData)
+				PurchaseService.create($scope.formData)
 
 					// if successful creation, call our get function to get all the new todos
 					.success(function(data) {
 						$scope.loading = false;
-						$scope.formData = {}; // clear the form so our user is ready to enter another
-						$scope.todos = data; // assign our new list of todos
+					//	$scope.formData = {}; // clear the form so our user is ready to enter another
+						$scope.itemList = data; // assign our new list of todos
+            $state.go("tabs.list");
 					});
 			}
 		};
 
   
+  }).controller('listCtrl', function($scope,$state,PurchaseService) {
+  
+    $scope.loading = true;
+
+    // GET =====================================================================
+    // when landing on the page, get all todos and show them
+    // use the service to get all the todos
+    PurchaseService.get()
+      .success(function(data) {
+        debugger;
+        $scope.itemList = data;
+        $scope.loading = false;
+      });
+
+    // CREATE ==================================================================
+    // when submitting the add form, send the text to the node API
+    
+  
   })
+
+
+
