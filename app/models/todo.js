18a034891e5a5bd2f728d6941a0f3f408ee64237
chamarasanjeewa <chamara.sanjeewa@gmail.com>
2015-12-07 08:52:46 -0800

    ddd

--- app/models/todo.js
+++ app/models/todo.js
@@ -7,16 +7,16 @@ module.exports = mongoose.model('Todo', {
 	 date    : Date
 });
 
-module.exports = mongoose.model('Users', {
-	username : {type : String, default: ''},
-	password: {type : String, default: ''},
-	createdDate    : Date,
-    updatedDate:Date
-});
+// module.exports = mongoose.model('Users', {
+// 	username : {type : String, default: ''},
+// 	password: {type : String, default: ''},
+// 	createdDate    : Date,
+//     updatedDate:Date
+// });
 
-module.exports = mongoose.model('purchased', {
-	text : {type : String, default: ''},
-	amount: Number,
-	userId:{type : String, default: ''},
-	 date    : Date
-});
+// module.exports = mongoose.model('purchased', {
+// 	text : {type : String, default: ''},
+// 	amount: Number,
+// 	userId:{type : String, default: ''},
+// 	 date    : Date
+// });
