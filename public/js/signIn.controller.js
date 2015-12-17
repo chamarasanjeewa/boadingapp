91b6b19d9fbb20291480678cf03877878d3b4e10
chamarasanjeewa <chamara.sanjeewa@gmail.com>
2015-12-17 08:15:42 -0800

    added

--- public/js/signIn.controller.js
+++ public/js/signIn.controller.js
@@ -4,28 +4,7 @@ angular.module('boadingBudgetApp').controller('SignInCtrl',SignInCtrl);
 SignInCtrl.$inject = ['$scope','$state','PurchaseService'];
 
 function SignInCtrl($scope, $state,PurchaseService) {
-  $scope.loginError=null;
-  $scope.signIn = function(user) {
 
-    PurchaseService.signIn(user)
-			.success(function(data) {
-				 $state.go('tabs.home');
-			}).error(function(data) {
-            $scope.loginError="User name or password invalid";
-				console.log('unauthorized ')
-			});
-   
-  };
- 
-  $scope.register = function(user) {
-    PurchaseService.register(user)
-			.success(function(data) {
-				 $state.go('tabs.home');
-			}).error(function(data) {
-
-			});
-   
-  };
   
 };
 })();
