beb7faaf9bbd77556e859f9a831457aca6416354
chamarasanjeewa <chamara.sanjeewa@gmail.com>
2015-12-18 03:51:15 -0800

    added

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 module.exports = {
 
 	// the database url to connect
+	url :'mongodb://chamara:sanjusanju@ds033484.mongolab.com:33484/bmann'
 	//url : 'mongodb://chamara:sanjusanju@ds061464.mongolab.com:61464/boadingdb'
-	url : 'mongodb://127.0.0.1:27017/bmann'
+	//url : 'mongodb://127.0.0.1:27017/bmann'
 
 }
