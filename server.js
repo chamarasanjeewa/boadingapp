47d8b2961e5c4fde69783a840ce12f46482a7719
chamarasanjeewa <chamara.sanjeewa@gmail.com>
2016-01-12 07:52:41 +0530

    added

--- server.js
+++ server.js
@@ -42,7 +42,9 @@ app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Headers","Access-Control-Allow-Headers")
+})
 //test code
 // routes ======================================================================
 require('./app/routes.js')(app);
